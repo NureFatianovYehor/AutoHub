acdef1a1049e93aac322fd4b11fd62463e62dac9
Mordin228 <yehor.fatianov@nure.ua>
2025-06-02 15:49:26 +0200

    init: start AutoHub backend

---
+++ index.js
@@ -0,0 +1,15 @@
+// index.js
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('AutoHub API is running');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
